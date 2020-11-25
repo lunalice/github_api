d36746fcf0d841ad55b8b398355038e9db386c7c
lunalice <yamashita0120@gmail.com>
2020-11-25 17:24:14 +0900

    tweak

--- request.sh
+++ request.sh
@@ -9,4 +9,4 @@ END_DAY=${INPUT_2:0:4}-${INPUT_2:4:2}-${INPUT_2:6:2}
 RANGE="${START_DAY}..${END_DAY}"
 AUTHORIZATION="Authorization: token ${ACCESS_TOKEN}"
 ACCEPT="Accept: application/vnd.github.v3+json"
-curl -H "${ACCEPT}" -H "${AUTHORIZATION}" "https://api.github.com/search/issues?q=is:pr+author:${USER}+updated:${RANGE}" | jq -sr ".[].items[] | [.title, .html_url, .user.login, .updated_at] | @csv" > output.txt
+curl -H "${ACCEPT}" -H "${AUTHORIZATION}" "https://api.github.com/search/issues?q=is:pr+author:${USER}+updated:${RANGE}" | jq -sr '.[].items[] | [.title, .html_url, .user.login, .updated_at, "#" + (.number|tostring)] | @tsv' > output.txt
